e964e1897f1674829bce8f0def2e8848afe10168
cburgoin-dev <cristian.burgoin.dev@gmail.com>
2026-05-25 22:54:44 -0700

    Quick fix

--- app/src/main/java/com/example/lostfoundapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/lostfoundapp/ui/screens/LoginScreen.kt
@@ -204,7 +204,26 @@ fun LoginScreen(
                     PrimaryButton(
                         text = "Iniciar sesión",
                         onClick = {
-                            onLoginClick()
+                            viewModelScope.launch {
+
+                                val response = api.login(
+                                    email = "dev@dev.com",
+                                    password = "password"
+                                )
+
+                                if (response.isSuccessful) {
+
+                                    val token = response.body()
+
+                                    onLoginClick()
+
+                                    Log.d("TOKEN", token ?: "null")
+
+                                } else {
+
+                                    Log.d("LOGIN", "Error")
+                                }
+                            }
                         }
                     )
 
